f4089835b4c65e3882cba519d14821995a661a44
Makkuhuqueriza <mhpena@up.edu.ph>
2023-12-28 22:56:34 +0800

    finish case 1 in admin panel

--- mini-voting-system.c
+++ mini-voting-system.c
@@ -19,12 +19,15 @@ struct currentValidID currentValidID;
 CANDIDATE candidateArray[20]; 
 int numberOfCandidates; 
 char studentVotes[200];
-void adminPanel();
+
 
 void display();
 int choiceforUser();
-int isValid(char userID[15]);
+void adminPanel();
 int authenticateAdmin();
+void initiateNewElection();
+void saveElectionInfoInFile();
+void createCandidateFiles();
 
 int main(){
 
@@ -36,11 +39,9 @@ int main(){
         switch(choice){
             case '1':
                 // studentPanel();
-                printf("1");
                 break;
             case '2':
                 adminPanel();
-                printf("2");
                 break;
             case '0':
                 printf("\nExiting program...");
@@ -73,54 +74,36 @@ int choiceforUser(){
     return choice;
 }
 
-int authenticateAdmin(){
-    char username[15], password[10];
-    
-    printf("\nEnter username(Max of 14 characters): ");
-    scanf("%s",username);
-    if((strcmp(username,"Admin"))!=0)
-        return 0;
-    else
-    {
-        printf("Enter Password(Max of 9 characters): ");
-        int i=0;
-        for(i=0;i<5;i++)
-        {
-            password[i]=getch();    
-            printf("%c",'*');
-        }
-        password[i]='\0';
-        if((strcmp(password,"pasadopls"))!=0)
-            return 0;
-    }
-    return 1;
-}
-
 void adminPanel(){
     while(1){
         
         if(authenticateAdmin()!=1){
-            printf("\n Wrong Username or Password \n");            
+            printf("\nWrong Username or Password (Press Enter)");
+            getch();
+            system("cls");
+            display();            
             break;
         }
 
         printf("\n\nLOGGED IN SUCCESSFULLY (Press Enter)");
 		getch();
+        system("cls");
 
         while(1)
         {
             char inputID[15];
-            char input;char banInp;
+            char input; char banInp;
             int WinnerCid, totalVotedNow=0;
-            printf("\n1.New Election\n2.Continue Previous Election\n3.Delete Illegal Vote\n4.Ban User IDs\n5.Result\n6.Logout\nOption:");
+            printf("\n\tUSER CHOICES (Select among the options available)");
+            printf("\n\t\t[1] New Election\n\t\t[2] Continue Previous Election\n\t\t[3] Delete Illegal Vote\n\t\t[4] Ban User IDs\n\t\t[5] Result\n\t\t[6] Logout\n\tChoice: ");
 			scanf(" %c",&input);
 
             switch(input)
             {
                 case '1':
-                    // initiateNewElection();
-                    // saveElectionInfoInFile();
-                    // createCandidateFiles();
+                    initiateNewElection();
+                    saveElectionInfoInFile();
+                    createCandidateFiles();
                     break;
                 case '2':
                     // loadElectionInfoFromFile();
@@ -153,7 +136,7 @@ void adminPanel(){
                     // printf("\nVoting Percentage: %d %%\n\n",(totalVotedNow*100)/currentValidID.totalVoters);
                     break;
                 case '6':
-                    return;
+                    return; 
                 default:
                     printf("Invalid Option");
 					getch();
@@ -161,19 +144,94 @@ void adminPanel(){
 			
         }
     }
+}
 
-};
-
-
-// int isValid(char userID[15]){
-//     if(strlen(userID)!=14)
-//         return 0;
+int authenticateAdmin(){
+    char username[15], password[10];
     
-//     int inputedYear=extractYear(userID);
-//     int inputedRollNo = extractRollNo(userID);
-    
-//     if(inputedYear!=currentValidID.year || checkBranchCode(userID)!=1 || inputedRollNo>currentValidID.totalVoters)
-//         return 0;
+    printf("\nEnter username(Max of 14 characters): ");
+    scanf("%s",username);
+    if((strcmp(username,"Admin"))!=0)
+        return 0;
+    else
+    {
+        printf("Enter Password(Max of 9 characters): ");
+        int i=0;
+        for(i=0;i<9;i++)
+        {
+            password[i]=getch();    
+            printf("%c",'*');
+        }
+        password[i]='\0';
+        if((strcmp(password,"pasadopls"))!=0){
+            printf("\n");
+            return 0;
+        }
+    }
+    return 1;
+}
 
-//     return 1;
-// }
+void initiateNewElection(){
+    printf("\nNew Election Initiation:\n");
+    
+    printf("\nElections for which Year: ");
+    scanf("%d",&currentValidID.year);
+    printf("Enter branch code:");
+    scanf("%s",currentValidID.branch);
+    printf("Enter maximum number of voters:");
+    scanf("%d",&currentValidID.totalVoters);
+    printf("Enter the no. of candidates:");
+    scanf("%d",&numberOfCandidates);
+
+   for (int i = 0; i < currentValidID.totalVoters; i++)
+    {
+        studentVotes[i] = '0';
+    }
+
+    for (int i = 0;i < numberOfCandidates; i++) // Number of candidates is already pre-determined with maximum of 20 candidates
+    {
+        candidateArray[i].cid=i+1;
+        printf("Enter name of candidate %d: ",i+1);
+        scanf(" %s",candidateArray[i].cname);
+        candidateArray[i].votes=0;
+    }
+    return;
+}
+
+void saveElectionInfoInFile(){
+    printf("Saving Election Info in File...\n");
+    FILE *fpointer = fopen("ElectionInfo.txt", "w");
+    if(fpointer==NULL)
+    {
+        printf("\nError in file creation\n");
+        fclose(fpointer);
+        return;
+    }
+    fprintf(
+        fpointer,"%d\n%s\n%d\n%d",
+        currentValidID.year,
+        currentValidID.branch,
+        currentValidID.totalVoters,
+        numberOfCandidates
+    );
+    fclose(fpointer);
+    printf("Saved Successfully :>");
+}
+
+void createCandidateFiles(){
+    printf("\nCreating candidate files...\n");
+    FILE *fp;
+	char filename[20];
+    for(int i = 1;i <= numberOfCandidates; i++){
+        sprintf(filename,"candidate%d.txt",i);
+		fp=fopen(filename,"w");
+        fprintf(
+            fp,"0\n%s",
+            candidateArray[i-1].cname
+        );
+		fclose(fp);
+    }
+    printf("Created Files successfully\n");
+}
+
+
