6dc0f14b9906b9404f9bd056fe11b934d52948a4
Makkuhuqueriza <mhpena@up.edu.ph>
2023-12-30 15:36:28 +0800

    done with all the admin panel - can still be improve

--- mini-voting-system.c
+++ mini-voting-system.c
@@ -20,7 +20,6 @@ CANDIDATE candidateArray[20];
 int numberOfCandidates; 
 char studentVotes[200];
 
-
 void display();
 int choiceforUser();
 void adminPanel();
@@ -28,6 +27,13 @@ int authenticateAdmin();
 void initiateNewElection();
 void saveElectionInfoInFile();
 void createCandidateFiles();
+void loadElectionInfoFromFile();
+void deleteIllegalVote(char userID[15]);
+void banID();
+int getWinner();
+int extractYear(char userID[15]);
+int extractRollNo(char userID[15]);
+int checkBranchCode(char userID[15]);
 
 int main(){
 
@@ -91,7 +97,7 @@ void adminPanel(){
 
         while(1)
         {
-            char inputID[15];
+            char inputID[15]; int i;
             char input; char banInp;
             int WinnerCid, totalVotedNow=0;
             printf("\n\tUSER CHOICES (Select among the options available)");
@@ -106,34 +112,34 @@ void adminPanel(){
                     createCandidateFiles();
                     break;
                 case '2':
-                    // loadElectionInfoFromFile();
+                    loadElectionInfoFromFile();
                     break;
                 case '3':
-                    // printf("\nEnter user ID to delete its vote: ");
-                    // scanf("%s",inputID);
-                    // deleteIllegalVote(inputID);
+                    printf("\nEnter user ID to delete its vote: ");
+                    scanf("%s",inputID);
+                    deleteIllegalVote(inputID);
                     break;
                 case '4':
-                    // printf("Do you want to ban particular ID's?\nPress 1 if yes or any other key to continue...");
-                    // scanf(" %c",&banInp);
-                    // if(banInp=='1'){
-                    //     banID();
-                    // }
+                    printf("Do you want to ban particular ID's?\nPress [1] for YES\nAny other key to continue...");
+                    scanf(" %c",&banInp);
+                    if(banInp=='1'){
+                        banID();
+                    }
                     break;
                 case '5':
-                    // WinnerCid = getWinner();
-                    // if(WinnerCid != -1){
-                    //     printf("\nWinner is %s with %d votes\n",candidateArray[WinnerCid-1].cname,candidateArray[WinnerCid-1].votes);
-                    // }
-                    // else{
-                    //     printf("\nIts A TIE");
-                    // }
-                    // printf("\nFull Result\n");
-                    // for(int i=0;i<numberOfCandidates;i++){
-                    //     totalVotedNow+=candidateArray[i].votes;
-                    //     printf("%d. %s -> %d votes\n",candidateArray[i].cid,candidateArray[i].cname,candidateArray[i].votes);
-                    // }
-                    // printf("\nVoting Percentage: %d %%\n\n",(totalVotedNow*100)/currentValidID.totalVoters);
+                    WinnerCid = getWinner();
+                    if(WinnerCid != -1){
+                        printf("\nWinner is %s with %d votes\n",candidateArray[WinnerCid-1].cname,candidateArray[WinnerCid-1].votes);
+                    }
+                    else{
+                        printf("\nIts A TIE");
+                    }
+                    printf("\nFull Result\n");
+                    for(i = 0;i<numberOfCandidates;i++){
+                        totalVotedNow+=candidateArray[i].votes;
+                        printf("%d. %s -> %d votes\n",candidateArray[i].cid,candidateArray[i].cname,candidateArray[i].votes);
+                    }
+                    printf("\nVoting Percentage: %d %%\n\n",(totalVotedNow*100)/currentValidID.totalVoters);
                     break;
                 case '6':
                     return; 
@@ -182,13 +188,14 @@ void initiateNewElection(){
     scanf("%d",&currentValidID.totalVoters);
     printf("Enter the no. of candidates:");
     scanf("%d",&numberOfCandidates);
-
-   for (int i = 0; i < currentValidID.totalVoters; i++)
+    
+    int i;
+   	for (i = 0; i < currentValidID.totalVoters; i++)
     {
         studentVotes[i] = '0';
     }
 
-    for (int i = 0;i < numberOfCandidates; i++) // Number of candidates is already pre-determined with maximum of 20 candidates
+    for (i = 0;i < numberOfCandidates; i++) // Number of candidates is already pre-determined with maximum of 20 candidates
     {
         candidateArray[i].cid=i+1;
         printf("Enter name of candidate %d: ",i+1);
@@ -222,7 +229,8 @@ void createCandidateFiles(){
     printf("\nCreating candidate files...\n");
     FILE *fp;
 	char filename[20];
-    for(int i = 1;i <= numberOfCandidates; i++){
+	int i;
+    for(i = 1;i <= numberOfCandidates; i++){
         sprintf(filename,"candidate%d.txt",i);
 		fp=fopen(filename,"w");
         fprintf(
@@ -233,5 +241,184 @@ void createCandidateFiles(){
     }
     printf("Created Files successfully\n");
 }
+
+void loadElectionInfoFromFile(){
+    FILE *f1,*f2,*f3;
+    f1=fopen("ElectionInfo.txt","r");
+    if(f1==NULL)
+        printf("Not Exist");
+    fscanf(f1,"%d",&currentValidID.year);
+    fseek(f1,2,SEEK_CUR);
+    fscanf(f1,"%s",currentValidID.branch);
+    fseek(f1,2,SEEK_CUR);
+    fscanf(f1,"%d",&currentValidID.totalVoters);
+    fseek(f1,2,SEEK_CUR);
+    fscanf(f1,"%d",&numberOfCandidates);
+    fclose(f1);
+   
+    //load candidates info and student votes
+    int i;
+    for (i = 0; i < currentValidID.totalVoters; i++)
+    {
+        studentVotes[i] = '0';
+    }
+    for(i = 1;i<=numberOfCandidates;i++)
+    {
+        int location;
+        char filename[20];
+        sprintf(filename,"candidate%d.txt",i);
+        f2=fopen(filename,"r+");
+        candidateArray[i-1].cid=i;
+        fscanf(f2,"%d",&candidateArray[i-1].votes);
+        fscanf(f2,"%s",candidateArray[i-1].cname);
+        while(!feof(f2)){
+            fscanf(f2,"%d",&location);
+            studentVotes[location-1] = i+48;
+        }
+        fclose(f2);
+    }
+
+    //load banned votes
+    int location;
+    f3=fopen("banned.txt","r+");
+    while(!feof(f3)){
+        fscanf(f3,"%d",&location);
+        studentVotes[location-1] = '$';
+    }
+    fclose(f3);
+    
+}
+
+void deleteIllegalVote(char userID[15])
+{
+    FILE *fp,*fcp;
+    char filename[20];
+    char line[20];
+
+    int location = extractRollNo(userID);
+    sprintf(filename,"candidate%d.txt",candidateArray[studentVotes[location-1]-49].cid);
+    candidateArray[studentVotes[location-1]-49].votes--;
+    studentVotes[location-1]='0';
+    if ((fp = fopen(filename,"r")) == NULL)    
+    {    
+        printf("\nFile cannot be opened...Operation Failed");
+        return;
+    }
+    printf("\nDeleting in process...\n ");    
+    if ((fcp = fopen("tmp.txt","w")) == NULL)    
+    {    
+        printf("\nFile cannot be opened...Operation Failed");
+        return;
+    }
+    
+    while (!feof(fp))
+    {
+        fscanf(fp,"%s",line);
+        fprintf(fcp,"%s\n",line);
+    }
+    fclose(fp);
+    fclose(fcp);
+    if ((fp = fopen(filename,"w")) == NULL)    
+    {    
+        printf("\nFile cannot be opened...Operation Failed");
+        return;
+    }
+    int numFromFile;
+    char cnameFromFile[20];
+    fcp = fopen("tmp.txt","r");  
+    fscanf(fcp,"%d",&numFromFile);
+    fprintf(fp,"%d",numFromFile-1);
+    fscanf(fcp,"%s",cnameFromFile);
+    fprintf(fp,"\n%s",cnameFromFile);
+    while(!feof(fcp)){
+        fscanf(fcp,"%d",&numFromFile);
+        if(numFromFile!=location)
+            fprintf(fp,"\n%d",numFromFile);
+    }
+    fclose(fp);
+    fclose(fcp);
+    remove("tmp.txt");
+    printf("\nVote deleted successfully\nPress any key to continue...");
+    getch();
+}
+
+void banID(){
+    printf("\nCreating Banned.txt...\n");
+    FILE *fp=fopen("Banned.txt", "w");
+    if(fp==NULL){
+        printf("Error: Banned.txt not created.\n");
+        fclose(fp);
+        return;
+    }
+    printf("Just Enter last roll no to ban\nPress 0 to exit... ");
+    int input;
+    while(1){
+        printf("\nEnter Number: ");
+        scanf("%d",&input);
+        if(input==0)
+            break;
+        studentVotes[input-1]='$';
+        fprintf(fp,"%d\n",input);
+    }
+    fclose(fp);
+    printf("Created Successfully\n");
+}
+
+int getWinner(){
+	int i;
+    int maxV = -1;
+    int winnerCid;
+    for(i = 0;i < numberOfCandidates; i++){
+        if(candidateArray[i].votes > maxV) {
+            winnerCid = candidateArray[i].cid;
+            maxV = candidateArray[i].votes;      
+        }
+        else if(candidateArray[i].votes == maxV) {
+            return -1;
+        }
+    }
+    return winnerCid;
+}
+
+int extractYear(char userID[15]){
+	
+	int i;
+    int year=0;
+    char tmp;
+    for(i = 0;i < 4; i++){
+        tmp=userID[i];
+		year=(year*10)+(tmp-48);
+    }
+    return year;
+}
+
+int extractRollNo(char userID[15]){
+	
+	int i;
+    int rollno=0;
+    char tmp;
+    for(i = 9; i < 14; i++){
+        tmp=userID[i];
+		rollno=(rollno*10)+(tmp-48);
+    }
+    return rollno;
+}
+
+//Will check whether the global branch code and inputed branch code is matching or not
+int checkBranchCode(char userID[15]){
+	
+	int i;
+    char branchCode[6];
+    for(i = 4; i < 9; i++){
+        branchCode[i-4]=userID[i];
+    }
+    branchCode[5]='\0';
+    if(strcmp(branchCode,currentValidID.branch)==0)
+        return 1;
+    else
+        return 0;
+}
+
+
 
 
